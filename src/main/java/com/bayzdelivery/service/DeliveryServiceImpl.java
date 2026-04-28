f289ad4ab4331c83e9ed234ddcac0211a3d480b7
Omar Ismail <omar.pal.95@gmail.com>
2026-04-28 22:32:01 +0300

    fix commission is never calculated

--- src/main/java/com/bayzdelivery/service/DeliveryServiceImpl.java
+++ src/main/java/com/bayzdelivery/service/DeliveryServiceImpl.java
@@ -2,8 +2,9 @@ package com.bayzdelivery.service;
 
 import java.util.Optional;
 
-import com.bayzdelivery.repositories.DeliveryRepository;
 import com.bayzdelivery.model.Delivery;
+import com.bayzdelivery.repositories.DeliveryRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,16 +12,25 @@ import org.springframework.stereotype.Service;
 public class DeliveryServiceImpl implements DeliveryService {
 
   @Autowired
-  DeliveryRepository deliveryRepository;
+  private DeliveryRepository deliveryRepository;
 
+  @Override
   public Delivery save(Delivery delivery) {
+
+    long price = delivery.getPrice() != null ? delivery.getPrice() : 0L;
+    long distance = delivery.getDistance() != null ? delivery.getDistance() : 0L;
+
+    // Commission = price * 0.05 + distance * 0.5
+    Long commission = (long) (price * 0.05 + distance * 0.5);
+
+    delivery.setComission(commission);
+
     return deliveryRepository.save(delivery);
   }
 
+  @Override
   public Delivery findById(Long deliveryId) {
     Optional<Delivery> optionalDelivery = deliveryRepository.findById(deliveryId);
-    if (optionalDelivery.isPresent()) {
-      return optionalDelivery.get();
-    }else return null;
+    return optionalDelivery.orElse(null);
   }
 }
