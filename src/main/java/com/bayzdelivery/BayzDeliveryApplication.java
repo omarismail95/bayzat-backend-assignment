1296932416365587875d28542e7518a6258ed407
Omar Ismail <omar.pal.95@gmail.com>
2026-04-28 22:37:40 +0300

    @EnableScheduling missing and @EnableAsync missing

--- src/main/java/com/bayzdelivery/BayzDeliveryApplication.java
+++ src/main/java/com/bayzdelivery/BayzDeliveryApplication.java
@@ -2,11 +2,15 @@ package com.bayzdelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class BayzDeliveryApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(BayzDeliveryApplication.class, args);
-  }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BayzDeliveryApplication.class, args);
+    }
 }
