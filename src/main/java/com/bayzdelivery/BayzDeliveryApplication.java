acc2787c4a10ee28f0c0ee3fd3a333550af3855a
Omar Ismail <omar.pal.95@gmail.com>
2026-04-28 23:16:48 +0300

    clean up main application class

--- src/main/java/com/bayzdelivery/BayzDeliveryApplication.java
+++ src/main/java/com/bayzdelivery/BayzDeliveryApplication.java
@@ -2,12 +2,13 @@ package com.bayzdelivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * Entry point for the BayzDelivery Spring Boot application.
+ *
+ * @author Omar Ismail
+ */
 @SpringBootApplication
-@EnableScheduling
-@EnableAsync
 public class BayzDeliveryApplication {
 
     public static void main(String[] args) {
