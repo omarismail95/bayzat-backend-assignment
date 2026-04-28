57d39491fc172bcc45020b6498fdf8757609ebb1
Omar Ismail <omar.pal.95@gmail.com>
2026-04-28 22:24:10 +0300

    PathVariable mismatch in PersonController

--- src/main/java/com/bayzdelivery/controller/PersonController.java
+++ src/main/java/com/bayzdelivery/controller/PersonController.java
@@ -3,38 +3,35 @@ package com.bayzdelivery.controller;
 import java.util.List;
 
 import com.bayzdelivery.model.Person;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import com.bayzdelivery.service.PersonService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
+@RequestMapping("/api/person")
 public class PersonController {
 
   @Autowired
-  PersonService personService;
+  private PersonService personService;
 
-  @PostMapping(path = "/api/person")
+  @PostMapping
   public ResponseEntity<Person> register(@RequestBody Person p) {
     return ResponseEntity.ok(personService.save(p));
   }
 
-  @GetMapping(path = "/api/person")
+  @GetMapping
   public ResponseEntity<List<Person>> getAllPersons() {
     return ResponseEntity.ok(personService.getAll());
   }
 
-  @GetMapping(path = "/api/person/{pers-id}")
-  public ResponseEntity<Person> getPersonById(@PathVariable(name="person-id", required=true)Long personId) {
+  @GetMapping("/{personId}")
+  public ResponseEntity<Person> getPersonById(@PathVariable("personId") Long personId) {
     Person person = personService.findById(personId);
     if (person != null) {
       return ResponseEntity.ok(person);
     }
     return ResponseEntity.notFound().build();
   }
-
 }
