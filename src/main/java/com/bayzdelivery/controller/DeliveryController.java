747a72bbc520fc320e22507254476b39f4b3b862
Omar Ismail <omar.pal.95@gmail.com>
2026-04-28 22:25:34 +0300

    DeliveryController missing base path mapping

--- src/main/java/com/bayzdelivery/controller/DeliveryController.java
+++ src/main/java/com/bayzdelivery/controller/DeliveryController.java
@@ -1,31 +1,30 @@
 package com.bayzdelivery.controller;
 
 import com.bayzdelivery.model.Delivery;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import com.bayzdelivery.service.DeliveryService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
+@RequestMapping("/deliveries")
 public class DeliveryController {
 
   @Autowired
-  DeliveryService deliveryService;
+  private DeliveryService deliveryService;
 
-  @PostMapping(path ="/delivery")
+  @PostMapping
   public ResponseEntity<Delivery> createNewDelivery(@RequestBody Delivery delivery) {
     return ResponseEntity.ok(deliveryService.save(delivery));
   }
 
-  @GetMapping(path = "/delivery/{delivery-id}")
-  public ResponseEntity<Delivery> getDeliveryById(@PathVariable(name="delivery-id",required=true)Long deliveryId){
+  @GetMapping("/{deliveryId}")
+  public ResponseEntity<Delivery> getDeliveryById(@PathVariable("deliveryId") Long deliveryId) {
     Delivery delivery = deliveryService.findById(deliveryId);
-    if (delivery !=null)
+    if (delivery != null) {
       return ResponseEntity.ok(delivery);
+    }
     return ResponseEntity.notFound().build();
   }
 }
